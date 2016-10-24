1cdb4e92794741f336ff56406300a43930437d43
Rachel Ralston <rachelralston@gmail.com>
2016-10-23 18:39:55 -0700

    organized files a bit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="nav">
+        Rachel Ralston
+        <div class="hamburger-box">
+          <div class="burger-line"></div>
+          <div class="burger-line"></div>
+          <div class="burger-line"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
