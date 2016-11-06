2e122e9b0e07e01760594b1f71a746863d49b670
Rachel Ralston <rachelralston@gmail.com>
2016-11-05 18:54:27 -0700

    setting up files for portfolio card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ocean from './ocean.jpg';
-// var BackgroundImg = require("./ocean.jpg")
 import './App.css';
 
 class App extends Component {
@@ -11,16 +10,18 @@ class App extends Component {
         <div className="left-nav">
           <h1>RACHEL</h1>
           <h1>RALSTON</h1>
-          Full Stack Engineer
+          <p>full stack engineer</p>
         </div>
         <div className="right-hero">
+          
           <div className="home-hero"></div>
           <img className="home-hero-img" src={ocean}></img>
         </div>
       </div>
-    );
-  };
-};
+    )
+  }
+}
+
 
 
 export default App;
---
+++ src/PortfolioCard.js
@@ -0,0 +1,12 @@
+import './PortfolioCard.css'
+
+class PortfolioCard extends Component {
+  render() {
+    <div className="portfolio-card">
+      <div className="portfolio-title"></div>
+      <div className="portfolio-image"></div>
+      <div className="portfolio-blurb"></div>
+      <div className="portfolio-link"></div>
+    </div>
+  }
+}
