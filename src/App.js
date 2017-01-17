4b99a129e09611c46cb398be03bb645e8683dbf6
Rachel Ralston <rachelralston@gmail.com>
2017-01-16 21:54:18 -0800

    honing content

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ class App extends Component {
       <StickyContainer>
         <Sticky>
           <div className="primary-nav">
-            <h1 id="rachelRalstonLogo">RACHEL RALSTON</h1>
             <div id="nav-link-box">
               <a className="nav-links" href="#">projects</a>
               <a className="nav-links" href="#">portfolio</a>
               <a className="nav-links" href="#">about</a>
             </div>
+            <h1 id="rachelRalstonLogo">RACHEL RALSTON</h1>
             <div id="social-links">
               <a href="mailto:rachel@rachelralston.com"><i className="fa fa-lg fa-envelope" aria-hidden="true"></i></a>
               <a href="https://github.com/rachel-ftw"><i className="fa fa-lg fa-github" aria-hidden="true"></i></a>
@@ -34,25 +34,26 @@ class App extends Component {
           </div>
         </Sticky>
         <div id="main">
-          <h1>I'm the unicorn that can <span>design & code</span> that you've been looking for. </h1>
           <div id="about">
-            <div id="about-words">
-              <p>Oakland based designer & full stack developer. I can handle whatever you
-              can throw at me. I've developed & maintained full stack apps, 
-              designed brand marketing strategies & materials, & illustrated 
-              magazine covers.</p>
-              <p>Non-tech loves include rock climbing, dancing, hiking with  
-              <a href={'./uma.jpg'}>this lady</a>, meditation and yoga.</p>
-            </div>
-          </div>
-          <div id="splash">
-            <div id="cta">
-              <a href="#" className="cta-button">check out my work</a>
-              <a href="#" className="cta-button">get in touch</a>
+            <div className="splash">
+              <div className="grey-background">
+                <h1>design & code</h1>
+                <div className="about-words">
+                  <p>Oakland based designer & full stack developer. I can handle whatever you
+                  can throw at me. I've developed & maintained full stack apps, 
+                  designed brand marketing strategies & materials, & illustrated 
+                  magazine covers.</p>
+                  <p>Non-tech loves include rock climbing, dancing, hiking with
+                  <a href={'./uma.jpg'}> this lady</a>, meditation and yoga.</p>
+                </div>
+                <div className="cta">
+                  <a href="#" className="cta-button">check out my work</a>
+                  <a href="#" className="cta-button">get in touch</a>
+                </div>
+              </div>
             </div>
           </div>
           <div id="project">{projectComponents}</div>
-          <div id="portfolio"></div>
         </div>
         </StickyContainer>
         <Footer/>
