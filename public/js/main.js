e5242b9bd8214ea66b5a9e852be5441dc7da0ad8
Rachel Ralston <rachelralston@gmail.com>
2017-04-11 15:45:44 -0700

    Got scrollmagic working and choose a new font for the splash

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
   var controller = new ScrollMagic.Controller();
   new ScrollMagic.Scene({
       triggerElement: '#splash', // starting scene, when reaching this element
-      duration: 100,    // the scene should last for a scroll distance of 100px
-      offset: 50        // start this scene after scrolling for 50px
+      duration: 50,    // the scene should last for a scroll distance of 100px
+      // offset: 50        // start this scene after scrolling for 50px
     })
     .setPin("#splash")
-    .addIndicators()
+    .addIndicators({
+      name: 'about fade in',
+      colorTrigger: 'black',
+      colorStart: '#75c695',
+      colorEnd: 'pink'
+    })
     .addTo(controller);
 })
