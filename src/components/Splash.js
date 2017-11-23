7413785c3f13dfad61d520972cb9b19345c37030
Rachel Ralston <rachelralston@gmail.com>
2017-11-23 23:45:28 +0000

    updating styling

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -4,8 +4,10 @@ import './Splash.css'
 const Splash = () => (
   <div className="splash">
     <div className="splash-grey-background">
-      <h1 className="splash-heading">RACHEL RALSTON</h1>
-      <p className="splash-sub-heading">full stack developer & designer.</p>
+      <div className="lemon-square">
+        <h1 className="splash-heading">RACHEL RALSTON</h1>
+        <p className="splash-sub-heading">blockchain, full stack & design.</p>
+      </div>
     </div>
   </div>
 )
