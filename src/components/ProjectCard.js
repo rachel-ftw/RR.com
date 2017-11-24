8e2dfa71c8255cd0032d688e8d624ec28b855ab4
Rachel Ralston <rachelralston@gmail.com>
2017-11-24 21:42:18 +0000

    polishing & reorganizing basic styling on portfolio cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,28 +1,16 @@
 import React from 'react'
+import './ProjectCard.css'
 
 
 const ProjectCard = (props) => {
-  const style = {
-    card: {
-      padding: '8px',
-      margin: '5px',
-      border: 'solid 2px yellow',
-    },
-    img: {
-      maxWidth: '32vw',
-      minWidth: '25vw',
-      minHeight: '40vh',
-      backgroundImage: `url('${props.image}')`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-    },
-  }
   return (
-    <div style={style.card}>
-      <div style={style.img}></div>
-      <div style={{padding: '5px 10px'}}>
-        <h3 style={{textAlign: 'center', fontSize: '1.5em'}}>{props.title}</h3>
+    <div className="project-card" >
+      <div
+        className="project-img"
+        style={{backgroundImage: `url('${props.image}')`}}
+      />
+      <div className="project-text-area">
+        <h3 className="project-title">{props.title}</h3>
         <p>{props.blurb}</p>
       </div>
     </div>
