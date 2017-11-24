4507a2742c0fe40c509f910e70e75286801f4f4d
Rachel Ralston <rachelralston@gmail.com>
2017-11-24 22:53:24 +0000

    adding contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button'
+
+const Contact = () => (
+  <div>
+    <h1>Want to work together?</h1>
+    <p>I&#39;m currently accepting new projects and would love to hear about yours. Please take a few minutes to tell me about it.</p>
+    <Button
+      color={'btn white'}
+      text={'GET STARTED'}
+      link={'things'}
+    />
+  </div>
+)
+
+export default Contact
