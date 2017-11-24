216c27b79d3528665aad3ac43fb28de74c176f34
Rachel Ralston <rachelralston@gmail.com>
2017-11-24 23:05:22 +0000

    Got basic contact component in there

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Button from './Button'
 
+import './Contact.css'
+
 const Contact = () => (
-  <div>
-    <h1>Want to work together?</h1>
-    <p>I&#39;m currently accepting new projects and would love to hear about yours. Please take a few minutes to tell me about it.</p>
+  <div className="container flex-column contact">
+    <h1 className="header secondary">Want to work together?</h1>
+    <p>I&#39;m currently accepting new projects and would love to hear about yours.</p>
+    <p>Please take a few minutes to tell me about it.</p>
     <Button
       color={'btn white'}
       text={'GET STARTED'}
