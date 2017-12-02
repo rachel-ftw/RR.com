d2590f64b05c9413e2fe143b9cd2c2fe800e4858
Rachel Ralston <rachelralston@gmail.com>
2017-12-02 15:29:46 -0800

    setting up redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './containers/App';
-import './styles/index.css';
+
 
 ReactDOM.render(
   <App />,
