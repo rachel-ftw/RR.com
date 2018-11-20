780f2456c4b4f94f1883941980507f84ef8dd598
Rachel Ralston <rachelralston@gmail.com>
2018-11-20 09:52:24 -0500

    rename splash

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
 import { projectsReducer } from './projects/reducer'
-import { blogReducer } from './blog/reducer';
+import { blogReducer } from './blog/reducer'
 
 const rootReducer = combineReducers({
   blogPosts: blogReducer,
