3e77a0cec34868b28348631d5d5339f21964269b
Rachel Ralston <rachelralston@gmail.com>
2018-11-20 11:12:00 -0500

    styling update on blog cards

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 import BlurbCard from './BlurbCard'
 
+const blogStyles = {
+  fontSize: '2em',
+  color: '#fff',
+}
+
 const BlogContainer = ({ blogPosts }) => {
   const blurbCards = blogPosts.map( post => <BlurbCard key={`post-${post.id}`} post={post} />)
+  
   return (
-  <div className="blog">
-    <h2>BLOG</h2>
+  <div className="blog" style={{ backgroundColor: '#333'}}>
+    <h2 style={blogStyles}>BLOG</h2>
     {blurbCards}
   </div>
 )}
--- src/components/blog/BlurbCard.js
+++ src/components/blog/BlurbCard.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-const BlurbCard = ({ post }) => {
-  const { title } = post
+import './BlurbCard.css'
 
+const BlurbCard = ({ post }) => {
+  const { title, blurb, image, publishDate } = post
+  console.log(post)
   return (
-    <h3>{title}</h3>
+    <div className="blurb-card" onClick={() => console.log('clicked!', title)}>
+      <div className="blurb-card-image" style={{ backgroundImage: `url('${image}')`}}></div>
+      <div className="blurb-card-content">
+        <h3>{title}</h3>
+        <p>{blurb}</p>
+        <div className="blurb-card-footer">
+          {publishDate}
+        </div>
+      </div>
+    </div>
   )
 }
 
