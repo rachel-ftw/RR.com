f423d6530166ecc6d017d7de0142bce2ba1ff81b
Rachel Ralston <rachelralston@gmail.com>
2018-11-22 17:03:34 -0500

    minor: verbiage refactor

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css'
 
 const Footer = () => (
   <div className="footer flex-column flex-center">
-    <span>this website was lovingly made by hand. </span>
-    <span>check out the <a href="https://www.github.com/rachel-ftw/portfolio-website" target="_blank" rel="noopener noreferrer">github</a> for details.</span>
+    <span>site lovingly made by hand. code on <a href="https://www.github.com/rachel-ftw/portfolio-website" target="_blank" rel="noopener noreferrer">github</a>.</span>
+    <span>©2019, Rachel Ralston</span>
   </div>
 )
 
