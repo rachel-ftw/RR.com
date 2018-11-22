87703734d78100d8506c12f564bd9a649d9404ca
Rachel Ralston <rachelralston@gmail.com>
2018-11-22 16:25:43 -0500

    moderate styling of modal content & button refactor

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 import './Button.css'
 
-const Button = ({ color, content, onClick }) => (
-  <div className={`btn ${color}`} onClick={() => onClick()}>{content}</div>
-)
+const Button = ({ color, content, onClick, size, flat }) => {
+  const classNames = [
+    'btn',
+    color,
+    flat ? 'flat' : '',
+    size ? size : 'medium'
+  ].join(' ')
 
+  return (
+    <div 
+      className={classNames} 
+      onClick={onClick ? () => onClick() : () => {}}>
+      {content}
+    </div>
+  )
+}
 
 export default Button
--- src/components/projects/ModalContent.js
+++ src/components/projects/ModalContent.js
@@ -1,20 +1,19 @@
 import React from 'react'
 
+import Button from '../common/Button'
+
 import './ModalContent.css'
 
 const ModalContent = ({ data }) => {
-  // console.log(data)
-  const { title, blurb, image} = data
+  const { title, blurb, type, technologies } = data
+  const techUsed = technologies.map((tech, index) => <Button key={`tech-${index}`} content={tech} size="small" flat />)
+  
   return (
-  <div className="project-container">
-    <h1>{title}</h1>
+  <div className="project-modal-container">
+    <span className="project-modal-type">{type}</span>
+    <h1 className="project-modal-title">{title}</h1>
+    <div className="project-modal-tech flex-row">{techUsed}</div>
     <p>{blurb}</p>
-    <div className="project-image">
-      <div 
-        className="project-image cover-image"
-        style={{ backgroundImage: `url('${image}')` }}
-      />
-    </div>
   </div>
 )}
 
