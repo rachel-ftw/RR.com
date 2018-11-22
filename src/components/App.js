851d653fe67594a0f71de37839d3bcce863f0bf4
Rachel Ralston <rachelralston@gmail.com>
2018-11-22 12:21:01 -0500

    delete navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-// import About from './about/About'
-// import BlogContainer from './blog/BlogContainer'
 import Footer from './footer/Footer'
-// import Navigation from './navigation/Navigation'
-import ProjectsContainer from './projects/ProjectsContainer'
 import Landing from './landing/Landing'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithubSquare } from '@fortawesome/free-solid-svg-icons'
+import ProjectsContainer from './projects/ProjectsContainer'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faGithubSquare } from '@fortawesome/free-solid-svg-icons'
 
 import './App.css'
 library.add(faGithubSquare)
@@ -20,7 +16,6 @@ class App extends Component {
     return (
         <div className="app">
           <Landing />
-          <FontAwesomeIcon icon="fab github-square" />
           <ProjectsContainer />
           <Footer />
         </div>
